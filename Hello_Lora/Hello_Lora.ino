788a521951cb94ecb11879f8affd2160b1472def
DESKTOP-QOGCJF1\TakuyaBoshita <takuya.boshtia@ucl.meijo-u.ac.jp>
2018-03-12 16:29:07 +0900

    Hello_LoRa Test

---
+++ Hello_Lora/Hello_Lora.ino
@@ -0,0 +1,23 @@
+#include <lorawan_client.h>
+#include <Arduino.h>
+
+LoRaWANClient client;
+
+void setup() {
+  Serial.begin(9600);
+  Serial.print("Connecting ... ");
+  if(! client.connect())
+  {
+    Serial.println(" failed to connect. Halt...");
+    for(;;){};
+  }
+}
+
+void loop() {
+  char *data="Hello! LoRa";
+
+  Serial.print("Sending ... ");
+  Serial.println(data);
+  client.sendData(data);
+  delay(10000);
+}
